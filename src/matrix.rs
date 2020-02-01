bd69233db7af1797c92b21282b2baeb5dfcc03df
Mark Smith <mark.smith@practicalpoetry.co.uk>
2020-02-01 11:47:35 +0000

    Matrix::with_values now panics instead of returning Result.

--- src/matrix.rs
+++ src/matrix.rs
@@ -19,18 +19,18 @@ impl Matrix {
         Self { rows, cols, data }
     }
 
-    pub fn with_values(rows: usize, cols: usize, values: Vec<f64>) -> Result<Self> {
+    pub fn with_values(rows: usize, cols: usize, values: Vec<f64>) -> Self {
         if values.len() != rows * cols {
-            return Err(anyhow!(
+            panic!(
                 "Length of values ({}) does not match matrix dimensions ({}x{})",
                 values.len(),
                 rows,
                 cols,
-            ));
+            );
         }
         let mut m = Self::new(rows, cols);
         m.data = values;
-        Ok(m)
+        m
     }
 
     pub fn value_at(self: &Self, row: usize, col: usize) -> Option<f64> {
@@ -53,7 +53,6 @@ impl Matrix {
                 1., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1.,
             ],
         )
-        .unwrap()
     }
 
     fn row(self: &Self, row: usize) -> Vec<f64> {
@@ -185,7 +184,6 @@ impl Matrix {
             4,
             vec![1., 0., 0., x, 0., 1., 0., y, 0., 0., 1., z, 0., 0., 0., 1.],
         )
-        .unwrap()
     }
 
     pub fn scaling(x: f64, y: f64, z: f64) -> Self {
@@ -194,7 +192,6 @@ impl Matrix {
             4,
             vec![x, 0., 0., 0., 0., y, 0., 0., 0., 0., z, 0., 0., 0., 0., 1.],
         )
-        .unwrap()
     }
 
     pub fn rotation_x(r: f64) -> Self {
@@ -220,7 +217,6 @@ impl Matrix {
                 1.,
             ],
         )
-        .unwrap()
     }
 
     pub fn rotation_y(r: f64) -> Self {
@@ -246,7 +242,6 @@ impl Matrix {
                 1.,
             ],
         )
-        .unwrap()
     }
 
     pub fn rotation_z(r: f64) -> Self {
@@ -272,7 +267,6 @@ impl Matrix {
                 1.,
             ],
         )
-        .unwrap()
     }
 }
 
@@ -381,7 +375,6 @@ impl Mul<Tuple> for Matrix {
 impl From<Tuple> for Matrix {
     fn from(t: Tuple) -> Self {
         Matrix::with_values(4, 1, vec![t.x(), t.y(), t.z(), t.w()])
-            .expect("Error creating Matrix from Tuple")
     }
 }
 
@@ -406,7 +399,7 @@ mod tests {
             vec![
                 1., 2., 3., 4., 5.5, 6.5, 7.5, 8.5, 9., 10., 11., 12., 13.5, 14.5, 15.5, 16.5,
             ],
-        )?;
+        );
 
         assert_eq!(m.value_at(0, 0), Some(1.0));
         assert_eq!(m.value_at(0, 3), Some(4.0));
@@ -420,7 +413,7 @@ mod tests {
 
     #[test]
     fn test_2x2_matrix() -> Result<()> {
-        let m = Matrix::with_values(2, 2, vec![-3., 5., 1., -2.])?;
+        let m = Matrix::with_values(2, 2, vec![-3., 5., 1., -2.]);
         assert_eq!(m.value_at(0, 0), Some(-3.0));
         assert_eq!(m.value_at(0, 1), Some(5.0));
         assert_eq!(m.value_at(1, 0), Some(1.0));
@@ -434,7 +427,7 @@ mod tests {
 
     #[test]
     fn test_3x3_matrix() -> Result<()> {
-        let m = Matrix::with_values(3, 3, vec![-3., 5., 0., 1., -2., -7., 0., 1., 1.])?;
+        let m = Matrix::with_values(3, 3, vec![-3., 5., 0., 1., -2., -7., 0., 1., 1.]);
         assert_eq!(m.value_at(0, 0), Some(-3.0));
         assert_eq!(m.value_at(1, 1), Some(-2.0));
         assert_eq!(m.value_at(2, 2), Some(1.0));
@@ -454,14 +447,14 @@ mod tests {
             vec![
                 1., 2., 3., 4., 5., 6., 7., 8., 9., 8., 7., 6., 5., 4., 3., 2.,
             ],
-        )?;
+        );
         let m2 = Matrix::with_values(
             4,
             4,
             vec![
                 1., 2., 3., 4., 5., 6., 7., 8., 9., 8., 7., 6., 5., 4., 3., 2.,
             ],
-        )?;
+        );
 
         assert_eq!(m1, m2);
         Ok(())
@@ -475,14 +468,14 @@ mod tests {
             vec![
                 1., 2., 3., 4., 5., 6., 7., 8., 9., 8., 7., 6., 5., 4., 3., 2.,
             ],
-        )?;
+        );
         let m2 = Matrix::with_values(
             4,
             4,
             vec![
                 2., 3., 4., 5., 6., 7., 8., 9., 8., 7., 6., 5., 4., 3., 2., 1.,
             ],
-        )?;
+        );
 
         assert_ne!(m1, m2);
         Ok(())
@@ -496,7 +489,7 @@ mod tests {
             vec![
                 1., 2., 3., 4., 5., 6., 7., 8., 9., 8., 7., 6., 5., 4., 3., 2.,
             ],
-        )?;
+        );
 
         let m2 = Matrix::with_values(
             4,
@@ -504,7 +497,7 @@ mod tests {
             vec![
                 -2., 1., 2., 3., 3., 2., 1., -1., 4., 3., 6., 5., 1., 2., 7., 8.,
             ],
-        )?;
+        );
 
         assert_eq!(
             (m1 * m2)?,
@@ -515,7 +508,7 @@ mod tests {
                     20., 22., 50., 48., 44., 54., 114., 108., 40., 58., 110., 102., 16., 26., 46.,
                     42.,
                 ],
-            )?
+            )
         );
 
         Ok(())
@@ -529,7 +522,7 @@ mod tests {
             vec![
                 1., 2., 3., 4., 2., 4., 4., 2., 8., 6., 4., 1., 0., 0., 0., 1.,
             ],
-        )?;
+        );
         let t = Tuple::new(1., 2., 3., 1.);
 
         assert_eq!(m1 * t, Tuple::new(18., 24., 33., 1.));
@@ -545,7 +538,7 @@ mod tests {
             vec![
                 0., 1., 2., 4., 1., 2., 4., 8., 2., 4., 8., 16., 4., 8., 16., 32.,
             ],
-        )?;
+        );
         assert_eq!((&m1 * &Matrix::identity4())?, m1);
         assert_eq!((m1.clone() * Matrix::identity4())?, m1);
 
@@ -560,7 +553,7 @@ mod tests {
             vec![
                 0., 9., 3., 0., 9., 8., 0., 8., 1., 8., 5., 3., 0., 0., 5., 8.,
             ],
-        )?;
+        );
 
         let transposed = Matrix::with_values(
             4,
@@ -568,7 +561,7 @@ mod tests {
             vec![
                 0., 9., 1., 0., 9., 8., 8., 0., 3., 0., 5., 5., 0., 8., 3., 8.,
             ],
-        )?;
+        );
 
         assert_eq!(m1.transpose(), transposed);
 
@@ -583,7 +576,7 @@ mod tests {
     #[test]
     fn test_determinant() -> Result<()> {
         assert_eq!(
-            Matrix::with_values(2, 2, vec![1., 5., -3., 2.])?.determinant(),
+            Matrix::with_values(2, 2, vec![1., 5., -3., 2.]).determinant(),
             17.
         );
         Ok(())
@@ -591,8 +584,8 @@ mod tests {
 
     #[test]
     fn test_submatrix_3x3() -> Result<()> {
-        let m1 = Matrix::with_values(3, 3, vec![1., 5., 0., -3., 2., 7., 0., 6., -3.])?;
-        let expected = Matrix::with_values(2, 2, vec![-3., 2., 0., 6.])?;
+        let m1 = Matrix::with_values(3, 3, vec![1., 5., 0., -3., 2., 7., 0., 6., -3.]);
+        let expected = Matrix::with_values(2, 2, vec![-3., 2., 0., 6.]);
 
         assert_eq!(m1.submatrix(0, 2)?, expected);
 
@@ -607,8 +600,8 @@ mod tests {
             vec![
                 -6., 1., 1., 6., -8., 5., 8., 6., -1., 0., 8., 2., -7., 1., -1., 1.,
             ],
-        )?;
-        let expected = Matrix::with_values(3, 3, vec![-6., 1., 6., -8., 8., 6., -7., -1., 1.])?;
+        );
+        let expected = Matrix::with_values(3, 3, vec![-6., 1., 6., -8., 8., 6., -7., -1., 1.]);
 
         assert_eq!(m1.submatrix(2, 1)?, expected);
 
@@ -617,7 +610,7 @@ mod tests {
 
     #[test]
     fn test_minor() -> Result<()> {
-        let a = Matrix::with_values(3, 3, vec![3., 5., 0., 2., -1., -7., 6., -1., 5.])?;
+        let a = Matrix::with_values(3, 3, vec![3., 5., 0., 2., -1., -7., 6., -1., 5.]);
         let b = a.submatrix(1, 0)?;
         assert_eq!(b.determinant(), 25.);
         assert_eq!(a.minor(1, 0)?, 25.);
@@ -627,7 +620,7 @@ mod tests {
 
     #[test]
     fn test_cofactor() -> Result<()> {
-        let a = Matrix::with_values(3, 3, vec![3., 5., 0., 2., -1., -7., 6., -1., 5.])?;
+        let a = Matrix::with_values(3, 3, vec![3., 5., 0., 2., -1., -7., 6., -1., 5.]);
         assert_eq!(a.minor(0, 0)?, -12.);
         assert_eq!(a.cofactor(0, 0)?, -12.);
         assert_eq!(a.minor(1, 0)?, 25.);
@@ -638,7 +631,7 @@ mod tests {
 
     #[test]
     fn test_determinant_3x3() -> Result<()> {
-        let a = Matrix::with_values(3, 3, vec![1., 2., 6., -5., 8., -4., 2., 6., 4.])?;
+        let a = Matrix::with_values(3, 3, vec![1., 2., 6., -5., 8., -4., 2., 6., 4.]);
         assert_eq!(a.cofactor(0, 0)?, 56.);
         assert_eq!(a.cofactor(0, 1)?, 12.);
         assert_eq!(a.cofactor(0, 2)?, -46.);
@@ -655,7 +648,7 @@ mod tests {
             vec![
                 -2., -8., 3., 5., -3., 1., 7., 3., 1., 2., -9., 6., -6., 7., 7., -9.,
             ],
-        )?;
+        );
         assert_eq!(a.cofactor(0, 0)?, 690.);
         assert_eq!(a.cofactor(0, 1)?, 447.);
         assert_eq!(a.cofactor(0, 2)?, 210.);
@@ -673,7 +666,7 @@ mod tests {
             vec![
                 6., 4., 4., 4., 5., 5., 7., 6., 4., -9., 3., -7., 9., 1., 7., -6.,
             ],
-        )?;
+        );
         assert_eq!(a.determinant(), -2120.);
         assert_eq!(a.invertible(), true);
 
@@ -688,7 +681,7 @@ mod tests {
             vec![
                 -4., 2., -2., -3., 9., 6., 2., 6., 0., -5., 1., -5., 0., 0., 0., 0.,
             ],
-        )?;
+        );
         assert_eq!(a.determinant(), 0.);
         assert_eq!(a.invertible(), false);
 
@@ -703,7 +696,7 @@ mod tests {
             vec![
                 -5., 2., 6., -8., 1., -5., 1., 8., 7., 7., -6., -7., 1., -3., 7., 4.,
             ],
-        )?;
+        );
         let b = a.inverse()?;
 
         assert_eq!(a.determinant(), 532.);
@@ -721,7 +714,7 @@ mod tests {
                     0.21805, 0.45113, 0.24060, -0.04511, -0.80827, -1.45677, -0.44361, 0.52068,
                     -0.07895, -0.22368, -0.05263, 0.19737, -0.52256, -0.81391, -0.30075, 0.30639
                 ]
-            )?
+            )
         );
 
         Ok(())
